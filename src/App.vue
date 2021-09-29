b397e2d91a5a024aa10584c2ae3edfe5421aaf07
João Bispo <joaobispo2077@gmail.com>
2021-09-28 22:10:24 -0300

    feat: add global styles

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,66 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+:root {
+  --green: #2d4a22;
+  --red: #e52e4d;
+  --blue: #5429cc;
+  --blue-light: #49aa26;
+  --text-title: #363f5f;
+  --text-body: #969cb3;
+  --input-background: #e7e9ee;
+  --input-border: #d7d7d7;
+  --background: #f0f2f5;
+  --shape: #ffffff;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%; // 15px
+  }
+
+  @media (max-width: 720px) {
+    font-size: 87.5%; // 14px
+  }
+}
+
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100%;
+  background: var(--background);
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body,
+input,
+textarea,
+button {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+strong {
+  font-weight: 600;
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity: 0.6;
+  cursor: not-allowed;
 }
 </style>
